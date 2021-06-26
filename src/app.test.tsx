4aee4ead2b250c3ec33166f300f835b0aac6f1a5
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-06-26 18:28:28 +0530

    Remove enzyme & use react-testing library

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
-import { shallow } from 'enzyme';
-import { expect } from 'chai';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import App from './app';
 
 describe('App', () => {
   it('renders the message', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.text()).to.have.string('Hello, world');
+    render(<App />);
+    return expect(screen.getByText('Hello, world!')).toBeInTheDocument();
   });
 });
